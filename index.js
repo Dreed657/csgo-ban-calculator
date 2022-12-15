729409796e2f25cdd3b65eca641c08b9d9106aef
Kristian Lachev <i435061@fhict.nl>
2022-12-15 21:50:29 +0200

    Added more information on processing
    Added simple use of spinner for UX

--- index.js
+++ index.js
@@ -1,31 +1,41 @@
-const playwright = require("playwright"); 
+const playwright = require("playwright");
+let Spinner = require('cli-spinner').Spinner;
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
-  });
+});
 
 const target = 'https://csgostats.gg/player/';
 
-readline.question('CSGO stats ID: ', (playerId) => { 
+readline.question('CSGO stats ID: ', (playerId) => {
     main(playerId);
     readline.close();
 });
 
 async function main(playerId) {
-    const browser = await playwright.chromium.launch({ 
-		headless: false, 
-	});
- 
-	const page = await browser.newPage(); 
-	await page.goto(target + playerId + '#/matches');
+    console.log(`Gathering information for player with id - ${playerId}`)
+
+    var spinner = new Spinner('%s');
+    spinner.setSpinnerString('|/-\\');
+    spinner.start()
+
+    const browser = await playwright.chromium.launch({
+        headless: false,
+    });
+
+    const page = await browser.newPage();
+    await page.goto(`${target}${playerId}#/matches`);
     await page.locator('data-testid=uc-accept-all-button').click();
+    spinner.clearLine()
+    console.log("Ordering data")
 
     const data = [];
     const tableRows = await page.$$('div#match-list-outer table tbody tr')
 
     let count = 0;
+
     for (let tr of tableRows) {
-        const rowContent = await tr.$$("td"); 
+        const rowContent = await tr.$$("td");
 
         const date = await rowContent[0].textContent();
         const isBanned = await rowContent[1].innerHTML();
@@ -41,9 +51,10 @@ async function main(playerId) {
         };
 
         data.push(row);
-        
         count++;
-	} 
+    }
+    spinner.clearLine();
+    spinner.stop();
 
     const total = data.length;
     const banned = data.filter((d) => d.isBanned).length;
@@ -54,11 +65,12 @@ async function main(playerId) {
     const chunkSize = total / ratio;
     const prevalence = banned / chunkSize;
 
+    console.log("\nComplete")
     console.table([
-        {name: 'Total', value: String(total)},
-        {name: 'Banned', value: String(banned)},
-        {name: 'Percentage', value: `${banPercentage.toFixed(2)}%`},
-        {name: 'Prevalence', value: `${prevalence.toFixed(0)} out of ${ratio}`}
+        { name: 'Total', value: String(total) },
+        { name: 'Banned', value: String(banned) },
+        { name: 'Percentage', value: `${banPercentage.toFixed(2)}%` },
+        { name: 'Prevalence', value: `${prevalence.toFixed(0)} out of ${ratio}` }
     ])
 
     process.exit(0);
